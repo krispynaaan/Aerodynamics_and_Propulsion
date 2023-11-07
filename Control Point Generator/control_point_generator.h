8631f85de473a98ce6635166e37bdaa91b19ab60
gitMaverickHub <133601671+gitMaverickHub@users.noreply.github.com>
2023-11-07 15:29:27 -0600

    Delete Control Point Generator directory

--- Control Point Generator/control_point_generator.h
+++
@@ -1,7 +0,0 @@
-#ifndef CONTROL_POINT_GENERATOR_H
-#define CONTROL_POINT_GENERATOR_H
-
-    void print(double (*arr)[2], int row, int col);
-    void cpg(double (*arr)[2], double (*originalPoints)[2], int row, int col); // control point generator
-
-#endif
